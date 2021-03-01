580d14ec468158bed44b4f482c5eff4855069d66
Justas383 <74533002+Justas383@users.noreply.github.com>
2021-03-01 18:22:22 +0200

    printing out directories and files

--- script.php
+++ script.php
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./style.css">
-</head>
-<body>
-
-<div class="table">
-<p>
-    Type
-</p>
-<p>
-    File name
-</p>
-<p>action</p>
-
 <?php
 $dirMas = scandir("C:\Program Files\Ampps\www\Sprint1");
 for($i = 0; $i < $dirMas; $i++){
@@ -33,11 +13,7 @@ for($i = 0; $i < $dirMas; $i++){
         <div class ="type"> ' . " File" . "<br>" . '</div>' );}
         else {break;}
 }
+
+
+
     ?>
-    </div>
-
-
-    
-    </div>
-</body>
-</html>
