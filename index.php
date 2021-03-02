57dc9d0b4ad2c337d50cb80aa8664c41bdfcb486
Justas383 <74533002+Justas383@users.noreply.github.com>
2021-03-02 15:55:00 +0200

    Structure changes

--- index.php
+++ index.php
@@ -9,34 +9,44 @@
 </head>
 <body>
 <?php
-$dirMas = scandir("C:\Program Files\Ampps\www\Sprint1");
+$dirMas = scandir('./');
 print_r($dirMas);
 ?>
 
-<div class="table">
-    <div class="header"> <p>File name</p></div>
-    <div class="header"> <p>File type</p></div>
-    <div class="header"> <p>Action</p></div>
+<table class = "table">
+    <tr class = "table">
+        <td>
+            File name
+        </td>
+        <td>
+            Type
+        </td>
+        <td>
+            Action
+        </td>
+    </tr>
 
 <?php
 for($i = 0; $i < $dirMas; $i++){
+    if($dirMas[$i] == '.' || $dirMas[$i] == '..')continue;
     if(is_dir($dirMas[$i])){
-        print_r( '
-        <div class="name">
-        <a href =C:\Program%Files\Ampps\www\Sprint1\\' . $dirMas[$i] . '>'.  $dirMas[$i] . '</a> </div> 
-        <div class ="type"> ' . "Directory"  . '</div>' . "<button> delete </button>");
+        print_r( '<tr class = "table">
+        <td><a href =./' . $dirMas[$i] . '>'.  $dirMas[$i] . '</a> </td> 
+        <td>Directory</td><td></td></tr>' );
     } else if(is_file($dirMas[$i])){
         print_r('
-        <div class="name">
-        <a href =C:\Program Files\Ampps\www\Sprint1\\' . $dirMas[$i] .'>' . $dirMas[$i] . '</a> </div>
-        <div class ="type"> ' . " File"  . '</div> ' .
-        "<button> delete </button>"
+       <tr class = "table">
+       <td> <a href =./' . $dirMas[$i] .'>' . $dirMas[$i] . '</a> </td>
+        <td> File</td>
+        <td ><button> delete </button></td></tr>'
     
     );}
         else {break;}
 }
     ?>
-    </div>
+
+
+</table>
   
 </body>
 </html>
