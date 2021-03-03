d7df269da5e279f0da47bb87f1ab81709cac63dc
Justas383 <74533002+Justas383@users.noreply.github.com>
2021-03-03 13:07:17 +0200

    fix

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ for($i = 0; $i < $dirMas; $i++){
     if($dirMas[$i] == '.' || $dirMas[$i] == '..')continue;
     if(is_dir($dirMas[$i])){
         print_r( '<tr class = "table">
-        <td ><a method="post" href =./' . $dirMas[$i] . '>'.  $dirMas[$i] . '</a> </td> 
+        <td ><a href =./' . $dirMas[$i] . '>'.  $dirMas[$i] . '</a> </td> 
         <td>Directory</td><td></td></tr>' );
     } 
     else if(is_file($dirMas[$i])){
