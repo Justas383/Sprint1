74b37abfa3829714584ce199372363961ba13efd
Justas383 <74533002+Justas383@users.noreply.github.com>
2021-03-07 22:19:19 +0200

    Fixed login, logout.  +upload, download, delete

--- index.php
+++ index.php
@@ -1,3 +1,61 @@
+<?php 
+//logout logika
+session_start();
+if(isset($_GET['action']) && $_GET['action'] == 'logout'){
+   session_start();
+   unset($_SESSION['uName']);
+   unset($_SESSION['pass']);
+   unset($_SESSION['logged_in']);
+   $_SESSION['logout_msg'] = '<p style="color: red;">You are logged out</p>';
+   header('Location:/' . end(explode('\\', getcwd())) .'/' );
+   exit;
+ }
+ //Login logika
+ if (isset($_POST['login']) && !empty($_POST['uName']) && !empty($_POST['pass'])){
+   if ($_POST['uName'] == 'Justas' && $_POST['pass'] == '123') {
+       $_SESSION['logged_in'] = true;
+       $_SESSION['timeout'] = time();
+       $_SESSION['uName'] = 'Justas';
+       header('Location: /' . end(explode('\\', getcwd())) . '/');
+   } else {
+       print_r('<div style="color:red">Neteisingi prisijungimo duomenys</div>');
+   }}
+   //upload logika
+   if (isset($_FILES['file'])){
+    if ($_FILES['file']['name'] === ""){
+        print('<div style="color: red;">No file selected to upload</div>');
+    } else {
+            $file_name = $_FILES['file']['name'];
+            $file_size = $_FILES['file']['size'];
+            $file_tmp = $_FILES['file']['tmp_name'];
+            $file_type = $_FILES['file']['type'];
+            move_uploaded_file($file_tmp, $_GET['path'] . $file_name);
+    }}
+    //download logika
+    if(isset($_POST['download'])){
+        $file='./' . $_POST['download'];
+        $fileToDownloadEscaped = str_replace("&nbsp;", " ", htmlentities($file, null, 'utf-8'));
+        ob_clean();
+        ob_start();
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/pdf'); 
+        header('Content-Disposition: attachment; filename=' . basename($fileToDownloadEscaped));
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($fileToDownloadEscaped)); 
+        flush();
+        readfile($fileToDownloadEscaped);
+        exit;
+    }
+    
+    //delete logika
+    if (isset($_POST['delete'])) {
+        unlink($_GET['path'] . $_POST['delete']);
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,26 +66,10 @@
     <link rel="stylesheet" href="./style.css">
 </head>
 <body> 
+
 <?php
-
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    if(!isset($_POST['uName']) || empty($_POST['uName'])){
-        print('Vartotojo vardo langelis tuscias<br>');}
-
-    if(!isset($_POST['pass']) || empty($_POST['pass'])){
-        print('Vartojo slaptazodzio langelis tuscias');}
-    }
-?>
-    
-<form action="" method="POST">
-    <label for="uName">Username:</label><br>
-    <input type="text" name="uName" value="<?php if(isset($_POST['uName'])) print($_POST['uName']) ?>">
-    <br>
-    <label for="lname">Password:</label><br>
-    <input type="password" id="pass" name="pass" value="<?php if(isset($_POST['pass'])) print($_POST['pass']) ?>">
-    <br>
-    <input type="submit" value="Submit">
-</form>
+    if ($_SESSION['logged_in'] === true) {
+    ?>
 <table>
     <!--FAILU NARSYKLE-->
 <?php
@@ -36,14 +78,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     print_r("Directory contents:" . $path );
     print("<br>");
 
-    //NEPAVYKO PADARYTI JOG NARSYKLE EITU GILIAU NEGU TRECIAS LYGIS
 for($i = 1; $i < count($pathArr); $i++){
     if($pathArr[$i] === '.' || $pathArr[$i] === '..'){continue;}
     else if(is_dir($path . '/' . $pathArr[$i])){
         if(isset($_GET['path'])){
         print_r( '
             <tr class = "table">
-            <td ><a href=' . $_SERVER['REQUEST_URI'] . $pathArr[$i] . '/>' . $pathArr[$i] . '</a> </td> 
+            <td ><a href="' . $_SERVER['REQUEST_URI'] . $pathArr[$i] . '/">' . $pathArr[$i] . '</a> </td> 
             <td>Directory</td><td></td></tr>');
             }
             else{
@@ -58,16 +99,35 @@ for($i = 1; $i < count($pathArr); $i++){
             <tr class = "table">
             <td> <a href="' . $_SERVER['REQUEST_URI'] . $pathArr[$i] . '/">' . $pathArr[$i] . '</a></td>
             <td> File</td>
-            <td><button href = class = "delete" action = "delete"> delete </button>
-            </td></tr>');}};
+            <td><form action="" method="POST">
+            <button type="submit" class="del" name="delete" value="' . $pathArr[$i] . '">Delete</button>
+            </form>
+            <form action="" method="POST">
+            <button type="submit" class="btn" name="download" value="' . $pathArr[$i] . '">Download</button>
+            </form></td></tr>');}};
 ?>
   </table>
   <!-- BACK MYGTUKAS -->
   <?php print('<button class="bck" onclick="history.go(-1);">Back</button>');?>
-<form action="" method="POST" >
-    <input class="newDir" type="text" id="newDir" placeholder="New Folder" name="newDir" value="<?php mkdir(""); ?>">
-    <br>
-    <input class = "newDir"type="submit" value="Submit">
-</form>
+ <div class="upload"> <form action="" method="POST" enctype="multipart/form-data">
+                <input type="file" id="file" name="file">
+                <input type="submit" class="upload-file-btn" name="upload" value="Upload file">
+            </form>
+ </div>
+  <!-- log-out-->
+  <div class="logout">
+            Press to <a class="logoutBtn" href="index.php?action=logout"> logout </a>
+        </div>
+  <?php } else { ?>
+  <!-- login-->
+  <div class="login">
+    <form action="" method="post">
+                <input type="text" name="uName" placeholder="Justas" required autofocus></br>
+                <input type="password" name="pass" placeholder="123" required>
+                <button class="btn" type="submit" name="login">Login</button>
+    </form>
+  </div>
+<?php } ?>
+
 </body>
 </html>
